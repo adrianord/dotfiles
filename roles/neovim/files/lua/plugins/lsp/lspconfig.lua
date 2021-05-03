3971a949259f8a63675483897a0051dc5238a15e
Adrian Ordonez <adrianord@gmail.com>
2021-05-03 13:04:38 -0500

    feat(neovim): add C-k C-d as a shortcut to format code

--- roles/neovim/files/lua/plugins/lsp/lspconfig.lua
+++ roles/neovim/files/lua/plugins/lsp/lspconfig.lua
@@ -27,9 +27,11 @@ local on_attach = function(client, bufnr)
   -- Set some keybinds conditional on server capabilities
   if client.resolved_capabilities.document_formatting then
     buf_set_keymap("n", "<space>fr", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    buf_set_keymap("n", "<C-k><C-d>", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
   end
   if client.resolved_capabilities.document_range_formatting then
     buf_set_keymap("v", "<space>fr", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+    buf_set_keymap("v", "<C-k><C-d>", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 end
 
